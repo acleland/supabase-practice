8b7b17a61c0f5c365ec6556e55e9972a7332c527
Andrea Cleland <andreaclelan@gmail.com>
2022-02-08 12:49:38 -0800

    worked on queries, not quite done

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
 1. Change `skip` to `test` in `fetch-utils.test.js` on one function at a time
 1. Complete the functions in `fetch-utils.js` using the Supabase client to get the test to pass
 1. Repeat steps 4-5 for every function
+
+api key:
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrd3VvaWZvcWp0ZHl1YW1scWJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQzNDE2MTAsImV4cCI6MTk1OTkxNzYxMH0.0bzW8RqDL090ne0FvaYqJLDBsUCtpfGm0oN6I-xwziw
+
+url:
+https://rkwuoifoqjtdyuamlqbt.supabase.co
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,7 +1,5 @@
-const SUPABASE_URL = "https://njovlynmvtgkoexzepbb.supabase.co";
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTMxNjU3MSwiZXhwIjoxOTU2ODkyNTcxfQ.4sFYy_xhbdh7hVgaEmH_ILGTIl6rsqtUgzAJsizIxkA";
-
+const SUPABASE_URL = 'https://rkwuoifoqjtdyuamlqbt.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrd3VvaWZvcWp0ZHl1YW1scWJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQzNDE2MTAsImV4cCI6MTk1OTkxNzYxMH0.0bzW8RqDL090ne0FvaYqJLDBsUCtpfGm0oN6I-xwziw';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 function checkError({ data, error }) {
@@ -10,24 +8,33 @@ function checkError({ data, error }) {
 
 export async function getMovies() {
   // return the list of all movies
-  const resp = await client.from("movies").select("*");
+  const resp = await client.from('movies').select('*');
   return checkError(resp);
 }
 
 export async function getMoviesWithDirector() {
   // return the list of all the movies with their director
+  const resp = await client.from('movies').select(`title, directors (name)`);
+  return checkError(resp);
 }
 
 export async function getDirectorNames() {
   // return the list of the director's names
+  const resp = await client.from('directors').select('name');
+  return checkError(resp);
 }
 
 export async function getMovieById(id) {
   // return the movie with the given id
+  const resp = await client.from('movies').select('*').eq('id', id).single();
+  console.log(resp)
+  return checkError(resp);
 }
 
 export async function getMovieByTitle(title) {
   // return the movie with the given title
+  const resp = await client.from('movies').select('*').eq('title', title).single();
+  return checkError(resp);
 }
 
 export async function getOldestMovie() {
